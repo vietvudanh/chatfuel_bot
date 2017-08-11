dcf3372c39afef073c046f9384353849b53b8bac
Viet Vu <vietvudanh@gmail.com>
2017-08-11 13:17:12 +0700

    update return for Chat fuel

--- init.sh
+++ init.sh
@@ -1,1 +1,3 @@
 heroku ps:scale web=1
+heroku buildpacks:set heroku/python
+heroku git:remote -a lunch
--- lunch/views/general.py
+++ lunch/views/general.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 import json
+import random
 
 from flask import current_app, Blueprint, render_template, jsonify
 general = Blueprint('general', __name__)
@@ -9,7 +11,23 @@ def index():
     data = None
     with open('lunch/data/places.json', 'r') as f:
         data = json.load(f)
+    places = data['places']
 
-    return jsonify(
-        data=data
-    )
+    total = sum([p['priority'] for p in places])
+    r = random.uniform(0, total)
+
+    # random with weight
+    choice = None
+    up_to = 0
+    for place in places:
+        if up_to + place['priority'] >= r:
+            choice = place
+            break
+        up_to += place['priority']
+
+    return jsonify({
+        'message': {
+            'text': "Địa điểm: " + choice['priority'],
+            'text': "Khoảng cách: " + choice['priority'],
+        }
+    })
